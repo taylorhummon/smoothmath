c0cb75552bae3954e87a6407f57ad20996755e56
Taylor Hummon <taylor.hummon@gmail.com>
2023-11-02 11:17:25 -0400

    update main scripts

--- python/src/forward_accumulation/main.py
+++ python/src/forward_accumulation/main.py
@@ -5,10 +5,10 @@ print("Forward Accumulation Method")
 # Example: Finding the partials of z = x * (x + y) + 5 * y * y at (x, y) = (2, 3)
 x = Variable()
 y = Variable()
-c = Constant(5)
-z = x * (x + y) + c * y * y
+z = x * (x + y) + Constant(5) * y ** Constant(2)
 variableValues = { x: 2, y: 3 }
-xPartial = z.derive(variableValues, x).partial
-yPartial = z.derive(variableValues, y).partial
-print("∂z/∂x =", xPartial) # Output: ∂z/∂x = 7
-print("∂z/∂y =", yPartial) # Output: ∂z/∂y = 32
+resultWithXPartial = z.derive(variableValues, x)
+resultWithYPartial = z.derive(variableValues, y)
+print("z =", resultWithXPartial.value)       # Output: z = 55
+print("∂z/∂x =", resultWithXPartial.partial) # Output: ∂z/∂x = 7
+print("∂z/∂y =", resultWithYPartial.partial) # Output: ∂z/∂y = 32
--- python/src/reverse_accumulation/main.py
+++ python/src/reverse_accumulation/main.py
@@ -5,10 +5,8 @@ print("Reverse Accumulation Method")
 # Example: Finding the partials of z = x * (x + y) + 5 * y * y at (x, y) = (2, 3)
 x = Variable()
 y = Variable()
-c = Constant(5)
-z = x * (x + y) + c * y * y
-variableValues = { x: 2, y: 3 }
-print("z =", z.evaluate(variableValues))         # Output: z = 55
-result = z.derive(variableValues)
+z = x * (x + y) + Constant(5) * y ** Constant(2)
+result = z.derive({ x: 2, y: 3 })
+print("z =", result.value)                       # Output: z = 55
 print("∂z/∂x =", result.partialWithRespectTo(x)) # Output: ∂z/∂x = 7
 print("∂z/∂y =", result.partialWithRespectTo(y)) # Output: ∂z/∂y = 32
