ec816297d6b8bdaa66a875ed12bb62ff4b7f49f4
Taylor Hummon <taylor.hummon@gmail.com>
2024-02-24 09:22:39 -0500

    remove path hacking

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,9 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import pathlib
-import sys
-sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 ### Project information
 
